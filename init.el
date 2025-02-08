1706d9538b6fdcd91f5d8020c59bed1750cd3b39
Gabriel M. Dutra <nulldutra@tuta.io>
2025-02-08 01:41:01 -0300

    Add package which-key

--- init.el
+++ init.el
@@ -37,13 +37,16 @@
 (use-package try
   :ensure t)
 
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(try use-package)))
+ '(package-selected-packages '(which-key try use-package)))
 
 ;;------- end package manager -------
 (custom-set-faces
